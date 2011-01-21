48eae2dae5205d6c767e296d995e93cd5007acbe
cowboycoded <john.mcaliley@gmail.com>
2011-01-21 15:55:22 -0500

    Add test rails app w/ cuc and rspec installed

---
+++ test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '450568519879517abfedbec1581dcf7de8a3cef3a206581b7663d72360f79fd46f6f7c7a7c42a95af58ed5e1f53187c5276321dc51466b739b31c3373737bb17'
